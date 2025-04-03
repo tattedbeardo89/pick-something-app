6e1af27c7db77875e3d5d222cd36b55a6362bdee
tattedbeardo89 <120513940+tattedbeardo89@users.noreply.github.com>
2025-04-02 19:22:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/pick-something-app/", // ✅ Correct for GitHub Pages
+  base: "/", // ✅ Correct for GitHub Pages
   define: {
     "process.env": {}, // ✅ Ensures compatibility
   },
